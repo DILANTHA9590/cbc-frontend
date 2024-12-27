85f5d37e41c2bc605eb4d524d299773962379a8b
DILANTHA9590 <dilanthanayanajith@gmail.com>
2024-12-27 21:12:19 +0530

    Add sidebar and dashboard link with icons in AdminPage

--- src/pages/adminpage.jsx
+++ src/pages/adminpage.jsx
@@ -1,4 +1,6 @@
 
+import { Link } from 'react-router-dom';
+import { BsGraphUp } from "react-icons/bs";
 
 export default function AdminPage(){
 
@@ -6,8 +8,31 @@ export default function AdminPage(){
 
         <>
 
+{/* //methana width eka full gannava evege h -screen eken divice eke height ekata venas venava */}
+        <div className="bg-slate-700  w-full h-screen flex">
 
-        <h1>admin page</h1>
+        
+                <div className=" w-[20%] h-screen bg-blue-800 flex flex-col items-center  ">
+
+
+
+                <Link to="/admin/dashboard"><BsGraphUp />Dashbord</Link>
+                <Link to="/admin/products">Products</Link>
+                <Link to="/admin/orders">Orders</Link>
+                <Link to="/admin/customer">Customer</Link>
+
+                    
+
+                </div>
+
+                <div className="w-[80%] h-screen bg-blue-400">
+
+                </div>
+                
+            </div>
+      
+
+
         
         </>
 

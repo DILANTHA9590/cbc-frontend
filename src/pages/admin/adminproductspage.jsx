2a2c00ad5f4b0dfc4469a656cd9d6261c1e84911
DILANTHA9590 <dilanthanayanajith@gmail.com>
2024-12-28 11:07:57 +0530

    create adminproductpage and get product details

--- src/pages/admin/adminproductspage.jsx
+++ src/pages/admin/adminproductspage.jsx
@@ -1,5 +1,26 @@
 
+ import axios from "axios"
+  //api me library eka insatall akaragathha meken tahamai backend ekata call
+ //karala dta genna ganna ekata apata meka oni meken venne bakend ekata api call gahanava
+
+
+ 
  export default function AdminProductsPage(){
+
+    //dan api methana component eka loard vena velavadi api call ekk ganava 
+
+    axios.get("http://localhost:3000/api/products").then((res)=>{//methana .then damme me deka vena venama thiyena 
+        //project dekka nisa koi veve euda dnne nane ekai
+        console.log(res);
+        
+        //habai mehema damma eorr ekk enava metahan apata broweser eke erorr ekk enava
+        //cores policy kiyala error ekk apiata ehema venne ape bkackend eka hamathanima  ena api codes piliganne na
+        // eka  piliganna apita npm install cores install karaganna oni eka backend ekata thama install karanne
+    })
+
+    // methandi api dan ape bacxkend eken data gannav metahana axios eken get dala thiyenne api 
+    //dan backeend eke dhadala 
+
     return(
 
         <>

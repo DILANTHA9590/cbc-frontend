a4a8a974ea40a3ed746153d8b44d56cf6d32e958
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-01-05 23:17:26 +0530

    Create Header component with navigation links and logo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,3 +1,44 @@
+import { Link } from "react-router-dom";
 export default function Header() {
-  return <header className="bg-accent w-full h-[50px]"></header>;
+  // dan api header component eka hadaganna thama yanne dan meka hadala api me component eka dannav
+  //avshya thanata api meka dagannava api php[ vala kare header footter ekas]
+  return (
+    // "bg-white
+    <header className="bg-white w-full h-[12vh] relative flex justify-center items-center ">
+      <img
+        src="/logo_.png"
+        className="h-[133px] w-[209px] rounded-full  cursor-pointer absolute left-[10px]"
+      />
+
+      <div className="h-full flex  items-center  justify-evenly w-[500px]">
+        <Link
+          to="/"
+          className=" text-accent font-bold text-xl hover:border-b border-b-accent"
+        >
+          Home
+        </Link>
+
+        <Link
+          to="/"
+          className=" text-accent font-bold text-xl hover:border-b border-b-accent"
+        >
+          Products
+        </Link>
+
+        <Link
+          to="/"
+          className=" text-accent font-bold text-xl  hover:border-b border-b-accent"
+        >
+          About Us
+        </Link>
+
+        <Link
+          to="/"
+          className=" text-accent font-bold text-xl  hover:border-b border-b-accent"
+        >
+          Contact Us
+        </Link>
+      </div>
+    </header>
+  );
 }

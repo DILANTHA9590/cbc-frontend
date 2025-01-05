3b64abb5e1eb6fe607af068b286adbb3c7fd742b
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-01-05 23:15:57 +0530

    Add custom color palette for primary, secondary, and accent colors in Tailwind CSS configuration🔴🟠🟡

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        //api fornt end eka design karanakota api main colors 3 k thoraganna oni eka api mehema dagganavaa
+        primary: "#eeebec",
+        secondary: "#444343",
+        accent: "#decd93",
+      },
+    },
   },
   plugins: [],
-}
+};

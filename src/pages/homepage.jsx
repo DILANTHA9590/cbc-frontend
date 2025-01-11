63d23dd8b9ca2832bce305e024b184c48d0a703c
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-01-11 10:21:53 +0530

    ✨ Added dynamic routing for ProductOverview component with `/productInfo/:id` 🌐

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,12 +1,29 @@
-import { Link, Routes } from "react-router-dom"; //limk use karaddi e kiyanne button nav valayrta vge link use karaddi meka
+import { Link, Route, Routes } from "react-router-dom"; //limk use karaddi e kiyanne button nav valayrta vge link use karaddi meka
 import Header from "../components/header";
+
+import ProductOverview from "./home/productOverview";
 //import karaganna oni
 
 export default function Homepage() {
   return (
-    <div className="h-screen w-screen bg-black border border-black border-[10px]">
+    <div className="h-screen w-screen">
       <Header />
-      <Routes path="/"></Routes>
+
+      <div className="w-full h-[calc(100vh-12vh)] bg-slate-800">
+        <Routes path="/">
+          <Route path="/dashboard" element={<h1>Dashbord</h1>} />
+
+          {/* // api packend eken param use karala id e kata adala eva baluva vage api methana product ekata adaala
+          // deval pennaganna ganna product id eka ella yavanava req ekath ekka ethakota e id eka magin e prodcut ekata adala
+          // data tika apata retrive karala denava */}
+          {/* dan methana id  vble eken meka loard vena velavat adala id eka backe ekata yavala apta eken deta 
+          retrive karala gannava .id   data eka pass vela enakota api eka gannava aran e vada tika karanne product overview page eken 
+          gannava  Product overviiew page eken itapasse e pr */}
+          <Route path="/productInfo/:id" element={<ProductOverview />} />
+
+          {/* <Route path="/dashboard" element={<h1>Dashbord</h1>} /> */}
+        </Routes>
+      </div>
     </div>
   );
 }

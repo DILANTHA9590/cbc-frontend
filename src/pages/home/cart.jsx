d59d16b5058ed1ee94e54f3b9a07e3249af81a71
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-01-14 14:47:16 +0530

    🛒 Fix cart page loading logic: updated useState, useEffect, and item rendering

--- src/pages/home/cart.jsx
+++ src/pages/home/cart.jsx
@@ -2,13 +2,21 @@ import { useEffect, useState } from "react";
 import { loardCard } from "../../utils/cartfunction";
 
 export default function Cart() {
-  const [cart, setCart] = (useState = []);
+  const [cart, setCart] = useState([]);
+
   useEffect(() => {
-    SlCreditCard(loardCard());
+    setCart(loardCard());
   });
+
   return (
     <div className="w-full h-full overflow-y-scroll flex flex-wrap justify-center">
-      <h1>Cart Page</h1>
+      {cart.map((item) => {
+        return (
+          <span>
+            {item.productId} X {item.qty}
+          </span>
+        );
+      })}
     </div>
   );
 }

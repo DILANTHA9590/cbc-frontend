bd23614e1ae8ce3c888ccf1eef6fd1c7cb314f8f
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-01-17 22:46:21 +0530

    🎨 Updated Tailwind CSS configuration: changed `accent` color and added a new `accentlight` color for better design flexibility. 🌈

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
         //api fornt end eka design karanakota api main colors 3 k thoraganna oni eka api mehema dagganavaa
         primary: "#eeebec",
         secondary: "#444343",
-        accent: "#decd93",
+        accent: "#cd7225",
+        accentlight: "#cd722590",
       },
     },
   },

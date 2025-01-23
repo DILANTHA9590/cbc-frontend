966d56901fa26f2c96e1c8cc0181443f21a28484
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-01-23 23:35:12 +0530

    enhance css for mobile renponsive

--- src/components/imageSlider.jsx
+++ src/components/imageSlider.jsx
@@ -11,13 +11,13 @@ export default function ImageSlider(props) {
   const images = props.images;
 
   return (
-    <div className="w-full aspect-square flex items-center flex-col relative">
+    <div className="relative flex flex-col items-center w-full aspect-square">
       {/* methandi api useate eke indexc eka default index eka methanatra pass karanav
        */}
       <img src={images[activeImage]} className="w-full h-full" alt="" />
       <div className="absolute w-full h-[100px] bottom-0 backdrop-blur-lg ">
         {/* ita passe ape propseeken ava images tika api map karala methanat adagannav */}
-        <div className="w-full h-full flex items-center justify-center overflow-hidden ">
+        <div className="flex items-center justify-center w-full h-full overflow-hidden ">
           {images.map((image, index) => (
             <img
               // methandi dan api set karala thiyenneokkoma ape image tika key pair valin ita passe api e adla
@@ -26,8 +26,7 @@ export default function ImageSlider(props) {
               onClick={() => setActiveImage(index)}
               key={index}
               src={image}
-              className="w-16 h-16 object-cover 
-                     mx-2 cursor-pointer"
+              className="object-cover w-16 h-16 mx-2 cursor-pointer"
             />
           ))}
         </div>
@@ -41,17 +40,16 @@ export default function ImageSlider(props) {
 // const images = props.images;
 // console.log(images);
 // return (
-//   <div className="w-full aspect-square flex items-center flex-col relative">
+//   <div className="relative flex flex-col items-center w-full aspect-square">
 //     <img src={images[0]} className="w-full h-full" alt="" />
 //     <div className="absolute w-full h-[100px] bottom-0 backdrop-blur-lg ">
 //       {/* ita passe ape propseeken ava images tika api map karala methanat adagannav */}
-//       <div className="w-full h-full flex items-center justify-center overflow-hidden">
+//       <div className="flex items-center justify-center w-full h-full overflow-hidden">
 //         {images.map((image, index) => (
 //           <img
 //             key={index}
 //             src={image}
-//             className="w-16 h-16 object-cover
-//                    mx-2 cursor-pointer"
+//             className="object-cover w-16 h-16 mx-2 cursor-pointer //"
 //           />
 //         ))}
 //       </div>

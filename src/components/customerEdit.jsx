c8bfe622d90a1e0d22ab0af8d9ef5f4e61c707a5
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-02-03 22:42:57 +0530

    create customer Edit Page📄

---
+++ src/components/customerEdit.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export default function CustomerEditPage() {
+  const [defaultUser, SetDefaultUser] = useState();
+  return (
+    <>
+      <h1>Customer Edit Page</h1>
+    </>
+  );
+}

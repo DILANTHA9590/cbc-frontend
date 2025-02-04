c347875839ff88c402cd2db374a1b409661be743
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-02-05 00:12:26 +0530

    🚧 Add profile pic upload logic (not finish)

--- src/components/customerEdit.jsx
+++ src/components/customerEdit.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import toast from "react-hot-toast";
 import { useLocation } from "react-router-dom";
+import uploadMedmiaToSupaBase from "../utils/mediaUpload";
 
 export default function CustomerEditPage() {
   const customer = useLocation().state.customer;
@@ -7,17 +9,40 @@ export default function CustomerEditPage() {
   console.log(location.email);
 
   const [email, setEmail] = useState(customer.email);
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [password, setPassword] = useState("updateuser");
+  const [confirmPassword, setConfirmPassword] = useState("updateuser");
 
   const [firstName, setFirstName] = useState(customer.firstName);
   const [LastName, setLastName] = useState(customer.lastName);
-  const [profilePic, setProfilePic] = useState();
+  const [profilePic, setProfilePic] = useState([]);
   console.log("fuckj;lj;", customer.profilePic);
 
   console.log("dddd", LastName);
 
-  async function clickUpdateBtn
+  async function clickUpdateBtn() {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      toast.error("Please Login First");
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setPassword("");
+      setConfirmPassword("");
+      toast.error("Passwords are different. Please re-enter");
+      return;
+    }
+
+    let imgUrl = customer.profilePic;
+    console.log("new image", profilePic);
+    const newImg = [];
+    if (profilePic.length > 0) {
+      for (let i = 0; i < profilePic.length; i++) {
+        newImg[i] = uploadMedmiaToSupaBase(profilePic[i]);
+      }
+    }
+  }
 
   return (
     <div className="max-w-lg p-6 mx-auto mt-10 bg-white rounded-lg shadow-lg sm:h-[75vh] h-full ">
@@ -89,16 +114,13 @@ export default function CustomerEditPage() {
           </label>
           <input
             type="file"
-            value={profilePic}
-            onChange={(e) => setProfilePic(e.target.value)}
+            onChange={(e) => setProfilePic(e.target.files[0])}
             className="w-full p-2 mt-1 border rounded-md focus:ring focus:ring-blue-300"
           />
         </div>
 
         <button
-          onClick={() => {
-            updateUser;
-          }}
+          onClick={clickUpdateBtn}
           className="w-full px-4 py-2 font-semibold text-white transition duration-300 bg-blue-600 rounded-md hover:bg-blue-700"
         >
           Update User

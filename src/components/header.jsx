8acdfbd1a51288e59de338e473e7b57a6d860018
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-02-06 21:32:42 +0530

    create home page📄

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,90 +1,23 @@
-import { Link } from "react-router-dom";
-import { RxHamburgerMenu } from "react-icons/rx";
-import { useState } from "react";
-import { RiCloseLargeFill } from "react-icons/ri";
-import NavSlidder from "./navSlider";
 export default function Header() {
-  // dan api header component eka hadaganna thama yanne dan meka hadala api me component eka dannav
-  //avshya thanata api meka dagannava api php[ vala kare header footter ekas]
-  const [isSliderOpen, setIsSliderOpen] = useState(false);
-
   return (
-    <>
-      {
-        //api methandi use satte ekk hadala thiyena ekde api state eka true nam vitharak me responsive nav bar eka pennava
-        //api methana code goda gahila thiyeddi kathine eka nisa api nav slider eka navslider compinent ekk athulata aran yanava
-        // nahtham kalin ethana thibba code eka thibbe methana ita passe ek component eka api  methanata pass karaganava
-        //meka kali thibba hati balanna oni nam navslidereka commonet e t eka &&( meke dan meda thiyenava eva ain karala)
-        //  e slider eke tjiyena coimpo eka danna
-
-        //e vagema dan apita close venakota venna oni de methanadi karaganna venne na mokada e thiyenne venama compionent ekakka
-        //e nisa api clse btn eka ebuvama venna oni de methandi pass karaganava onclose kiyala liyala
-        //function ekk pass karagannava ape states eka ME AND DENEN RETURN VENNE API MENU EKEA CLICK KARAMA
-        //API UDA STATE EKA TRU UE KARANAVA ETHAKOTA EKA RENDER VENAVA E VGEMA CLOSE VENNA API EKA YAVANAVA PROP EKK VIDIHATA NAV
-        //  SLIDER EKATA MOKDA EKA ETHANA THIYENNE E NISA EKA API PROP EKK VIDIHATA YAVANAVA E COMPO EKAKATA mokda slider eka
-        //call karana stete eka thiyenne meke apta methana onclick eka close venna denna vidihak na e nisa api me satte ekata
-        //kiyanava props vidihata yavanava function ekk ethakota api eke colse icon eka obapu
-        //state eka value eka false venava nav slioder eka hide venava
-
-        isSliderOpen && (
-          <NavSlidder
-            onCloseSlider={() => {
-              setIsSliderOpen(false);
-            }}
-          />
-        )
-      }
-      <header className="bg-white w-full h-[12vh]  flex justify-center items-center relative">
-        <img
-          src="/logo_.png"
-          className="h-[133px] w-[209px] rounded-full  cursor-pointer absolute left-[-30px]"
-        />
-
-        {/* //ita passe apita eka penna oni nam api haber btn eke isSliderOpen true kara ethakota elka click karama slider eka penava */}
-        <RxHamburgerMenu
-          className="absolute h-full text-3xl right-7 sm:hidden text-accent"
-          onClick={() => {
-            setIsSliderOpen(true);
-          }}
-        />
-
-        <div className="h-full   items-center  justify-evenly w-[500px]  sm:flex hidden">
-          <Link
-            to="/"
-            className="text-xl font-bold text-accent hover:border-b border-b-accent"
-          >
-            Home
-          </Link>
-
-          <Link
-            to="/products"
-            className="text-xl font-bold text-accent hover:border-b border-b-accent"
-          >
-            Products
-          </Link>
-
-          <Link
-            to=""
-            className="text-xl font-bold text-accent hover:border-b border-b-accent"
-          >
-            About Us
-          </Link>
-
-          <Link
-            to="/"
-            className="text-xl font-bold text-accent hover:border-b border-b-accent"
-          >
+    <div
+      className="flex items-center w-full h-full sm:h-[calc(100vh-20vh)]  mb-4 overflow-hidden bg-center bg-cover "
+      style={{ backgroundImage: "url('/header.jpg')" }}
+      id="Header "
+    >
+      <div className="container px-6 py-4 mx-auto text-center text-white md:px-20 lg:px-32 ">
+        <h2 className="inline-block text-5xl text-center text-transparent duration-700 sm:text-7xl animate-bounce bg-gradient-to-tr from-purple-800 to-red-500 via-amber-500 bg-clip-text">
+          Explore homes that fit your dreams
+        </h2>
+        <div className="mt-6 space-x-6">
+          <a href="#Projects" className="px-8 py-3 border border-white rounded">
+            Projects
+          </a>
+          <a href="#Context" className="px-8 py-3 bg-blue-500 rounded ">
             Contact Us
-          </Link>
-
-          <Link
-            to="/Cart"
-            className="text-xl font-bold text-accent hover:border-b border-b-accent"
-          >
-            Cart
-          </Link>
+          </a>
         </div>
-      </header>
-    </>
+      </div>
+    </div>
   );
 }

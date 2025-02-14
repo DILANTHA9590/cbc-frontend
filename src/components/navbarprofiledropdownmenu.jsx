0c6889d708f0661af91b011d9bdfca38d9f0668b
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-02-14 15:04:18 +0530

    ✅navigation to customer account page with dropdown menu close
     ✅ logout functionality with redirection to products page and token removal

--- src/components/navbarprofiledropdownmenu.jsx
+++ src/components/navbarprofiledropdownmenu.jsx
@@ -1,27 +1,36 @@
 import { MdKeyboardArrowUp } from "react-icons/md";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function NavBarProfilePictureDropDownMenu(props) {
   const navigate = useNavigate();
 
   function logOut() {
+    window.location.href = "/products";
     localStorage.removeItem("token");
-    navigate("/products"); // Redirect to products page after logout
+    // Redirect to products page after logout
   }
 
   function login() {
     navigate("/login");
   }
 
+  function navigateProfile() {
+    navigate("/customeraccount");
+    closeDropDownMenu();
+  }
+
   const closeDropDownMenu = props.clickToggleBtn;
 
   return (
-    <div className="absolute w-[120px] h-auto top-[51px] flex flex-col justify-center items-center bg-slate-300 rounded-md shadow-lg">
+    <div className="absolute w-[120px] h-auto top-[51px] flex flex-col justify-center items-center bg-slate-300 rounded-md shadow-lg backdrop:">
       <div className="absolute top-0 right-0 cursor-pointer">
         <MdKeyboardArrowUp onClick={closeDropDownMenu} />
       </div>
 
-      <button className="w-full py-2 font-bold text-center rounded-sm hover:bg-slate-500">
+      <button
+        className="w-full py-2 font-bold text-center rounded-sm hover:bg-slate-500"
+        onClick={navigateProfile}
+      >
         Profile
       </button>
 

0b74c084697dd70e4fc3eb6696ef46b4957ea439
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-02-14 22:06:16 +0530

    💡✨ Added "No products added" message when the list is empty

--- src/pages/home/cart.jsx
+++ src/pages/home/cart.jsx
@@ -95,45 +95,53 @@ export default function Cart() {
   }
 
   return (
-    <div className="flex flex-col w-full h-full overflow-y-scroll ">
-      <table className="w-full ">
-        <th>Product Image</th>
-        <th>Product Name</th>
-        <th className="hidden sm:block">Product Id</th>
-        <th>Product qty</th>
-        <th>Product Price</th>
-        <th>Total</th>
+    <>
+      {cart && cart.length > 0 ? (
+        <div className="flex flex-col w-full h-full overflow-y-scroll ">
+          <table className="w-full ">
+            <th>Product Image</th>
+            <th>Product Name</th>
+            <th className="hidden sm:block">Product Id</th>
+            <th>Product qty</th>
+            <th>Product Price</th>
+            <th>Total</th>
 
-        <td className="text-center"></td>
-        {cart.map((item) => {
-          return (
-            <CartCard
-              key={item.productId}
-              productId={item.productId}
-              qty={item.qty}
-            />
-          );
-        })}
-      </table>
-      <div className="flex flex-col items-end justify-center p-1 text-xl sm:text-3xl">
-        <h1 className="font-bold text-accent">
-          Total: LKR. {labeledTotal.toFixed(2)}
-        </h1>
-        <h1 className="font-bold text-accent">
-          Discount: LKR. {(labeledTotal - total).toFixed(2)}
-        </h1>
-        <h1 className="font-bold text-accent">
-          Grand Total: LKR. {total.toFixed(2)}
-        </h1>
-        <button
-          onClick={onOrderCheckoutClick}
-          className="bg-orange-500 sm:w-[300px] max-w p-2 rounded mt-2 hover:bg-orange-400
+            <td className="text-center"></td>
+            {cart.map((item) => {
+              return (
+                <CartCard
+                  key={item.productId}
+                  productId={item.productId}
+                  qty={item.qty}
+                />
+              );
+            })}
+          </table>
+          <div className="flex flex-col items-end justify-center p-1 text-xl sm:text-3xl">
+            <h1 className="font-bold text-accent">
+              Total: LKR. {labeledTotal.toFixed(2)}
+            </h1>
+            <h1 className="font-bold text-accent">
+              Discount: LKR. {(labeledTotal - total).toFixed(2)}
+            </h1>
+            <h1 className="font-bold text-accent">
+              Grand Total: LKR. {total.toFixed(2)}
+            </h1>
+            <button
+              onClick={onOrderCheckoutClick}
+              className="bg-orange-500 sm:w-[300px] max-w p-2 rounded mt-2 hover:bg-orange-400
         "
-        >
-          Checkout
-        </button>
-      </div>
-    </div>
+            >
+              Checkout
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="flex items-center justify-center h-full text-2xl text-gray-500">
+          No products added
+        </div>
+      )}
+    </>
   );
 }
 

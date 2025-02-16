23baf348905a01a08767e97b16d9c06f41203925
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-02-16 20:29:24 +0530

    remove Unnecessary comment and remove testing code✅

--- src/pages/loginpage.jsx
+++ src/pages/loginpage.jsx
@@ -5,24 +5,11 @@ import toast, { Toaster } from "react-hot-toast";
 import { useGoogleLogin } from "@react-oauth/google";
 
 export default function Loginpage() {
-  //mw hook eka api use karannne ape google login btn eka ebbuvama venna oni de
-  //meka apita katapadn karaganna oni na document eke thiye meka google auth eke npm doc vala
-  //eka kiyavannna
   const googleLogin = useGoogleLogin({
-    //apata me use google login hoiok eken labenne function ekk api eka dagaththa google loginvbkle kkata
-
-    // me function eka  api athulata denava json ekk
-    // login eka sucess unama ena response eka print karala denna kiyala
-    // dan api me function eka run karaganna oni google btn eka click karata passe
-    //e nisa api oncl;ick denna  oni  ggole btn ekata
-    // ita passe api log vela   api me page eken ma inspect baluvoth apata enava
-    //google eken  e ujsrta adala acces token ekak apta meken usrge visthara tika danagganna pulkuvan
-    // api etoken eka ekata ape bckend keat yavanna oni ekata api ekk hadaganna oni ape backend eke4
     onSuccess: (res) => {
       console.log(res);
       axios
         .post(import.meta.env.VITE_BACKEND_URL + "/api/users/google", {
-          //api meyage data tika yavana back ekata eka enne ape token eke acces token eke
           token: res.access_token,
         })
 
@@ -54,41 +41,18 @@ export default function Loginpage() {
   const [value, Setvalue] = useState();
 
   function login() {
-    //api dan me  port number eka calll karana eka methanin venathanakata daganna hdanne
-    //  .env ekkata mokda api den methana dirwectly thma backend url eka liyala thiyaggnne mamata meka host karanakota
-    //backend eka host karnnne venama thanaka front end eka host karanne venne venamathanaka
-    //front end eka host karanne venam thanaka ekama thanaka unath vena venama port dekka run karanana vei
-    //ethakota hadisiiye hari apita venama port number ekka  run karanna unoth me me 300 nathuva(vena port number ekka ) me axios call eken backend ekata
-    // call karana local host port number eka me hamathana venas karanna vena
-    //e nisa api meka .env eke dala veriable hdagena eka methanata denava ethakota eka tahanin vbenas unama hamathanama venas venava
-    //ara vidiha unama real backend url eken apata meka replace karanna venava ara vidihata dapuvbama eka lesi venas karanna
-    // mokda me url eka sthira deyak neme meka sari sare vans karanna sidda venava http://localhost:3000 venama .env ekk dagannava
-    // thiibe me vidihata  .post("http://localhost:3000/api/users/login", {
-    //dssas
     axios
 
       .post(import.meta.env.VITE_BACKEND_URL + "/api/users/login", {
         email: email, //uda thiyena email
         password: password,
-
-        //uda thiyena password ekai api postman eke req eka yauva vge thama me yavanne
-        //input type eke data tika aragena methanata dala userlogin eke email password ekata aragena backend ekata eyavanava
       })
       .then((res) => {
-        //ita passe ena res eka methanin balanava
         console.log(res);
-        // methandi balanava issella  user null da kiyala null nam eka navathinava natghtham phala kotasa exute karanava
 
         if (res.data.user == null) {
-          //methandi user kenek naththam apata backekend res eke eva tyika ganana puluvan  alert ekk viodihata
-          //naththama apata avashya ekeka daggnath puluva
           Setvalue(res.data.message);
 
-          //  naththam apata alert ekk denanath puluvan
-          // alert(res.data.message)//mehema alert dana eka dan kathai apata me vidihata dammoth deafult thiyena vidihata tha aelrt enne
-          // apata lassnata aler dagann use karanna puluan library ekk thyenava react hot toast library eka ganna puluvan
-          //eyalage site eken apata oni eva daganna puluvan
-          //e alert eka danne me vidihata
           toast.error(res.data.message);
 
           return;
@@ -96,22 +60,7 @@ export default function Loginpage() {
 
         toast.success(res.data.message);
 
-        //dan api methandi usertype eka balananna oni
-        // type eka admin nam admin page ekata yavanna oni type eka customer nam ciustomer mpage ekata yavanna oni
-        // dan apita meka check karaganna apata token eka oni e ka apata iisaraha vadavalata uvamana vensa nisa eka save karala thiyaganna
-        //oni apita eka nitharama request ekk yavdda oni venava
-
-        //api  me token eka local storage eke meka save karaganna me local storage eke ape crash eka save karanna puluvan ape web
-        //site ekata adalava
-
         localStorage.setItem("token", res.data.token);
-
-        //apiata browser eke token eka enne reponse eke data vala ena token eka api save kargannava token kiyana namin
-        //apita me local storage eke deval key value pairs vidihata me local storage eke deval save karaganna puluvan
-        //ethakota meka vercual vaguvak vge hithuvoth dan api methana token eka save karana key value pair vidihata token
-        //ekata adala eva string ekkin save venava strin ekkin haby
-
-        // dan api methana balanava user type eka balala eyalava page valata redirect venna
 
         if (res.data.user.type == "admin") {
           // type eka admin nam admin page ekata yavanava
@@ -120,16 +69,7 @@ export default function Loginpage() {
           window.location.href = "/";
         }
       });
-    // console.log("pressloginbtn");
-
-    // console.log(email)//dan api baluvgoth avith thiyenne realtime update una email eka eka balnna puluvan
-    //use state use karanne mekata inputfiled---> updateusesate ----->login btn dan mehema print karla vadak na apita ilga
-    //part ekata  yan
   }
-
-  //dan api meke obeject ekk vifdihata api dan balanava mokda api meke me component eke vanas vena attribute diha
-  // baluivoth email eka password keai meke vensa venava mokda api e vens vena hma ekkatama use state hadaganna
-  // oni
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
@@ -200,28 +140,3 @@ export default function Loginpage() {
     </div>
   );
 }
-
-// {
-//     customer mail
-
-//     "email": "customer123@example.com",
-//     "firstName": "John",
-//     "lastName": "Doe",
-//     "password": "securePassword123",
-//     "isBlock": false,
-//     "type": "customer",
-//     "profilePic": "https://example.com/profile-pic.jpg"
-
-//   }
-
-//     {
-//  admin detils
-//         "email": "admin123@example.com",
-//         "firstName": "John",
-//         "lastName": "Doe",
-//         "password": "securePassword123",
-//         "isBlock": false,
-//         "type": "admin",
-//         "profilePic": "https://example.com/profile-pic.jpg"
-
-//       }

aabce5842bd4b3d32a2ca2e381c829f550de9233
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-02-19 23:34:58 +0530

    create admin dashbord component

---
+++ src/pages/admin/adminDashBord.jsx
@@ -0,0 +1,11 @@
+export default function AdminDashboard() {
+  return (
+    <>
+      <h1>admin dashboard</h1>
+
+      <div className="w-screen h-screen bg-slate-500">
+        <div className="w-[400px] h-[400px]  bg-orange-300"></div>
+      </div>
+    </>
+  );
+}
